1b4eb6b9175b338f63b11b9220ebec1685b32dda
Suguru Motegi <suguru.motegi@gmail.com>
2016-10-24 22:21:05 -0700

    fix test cases

--- algorithms/Add Two Numbers/test.js
+++ algorithms/Add Two Numbers/test.js
@@ -26,24 +26,25 @@ function makeLinkNodes(array) {
 
 describe('#addTwoNumbers', () => {
 
-  it('(2 -> 4 -> 3 ) + (5 -> 6 -> 4) = (7 -> 0 -> 8)', () => {
-    const l1 = makeLinkNodes([2, 4, 3]);
-    const l2 = makeLinkNodes([5, 6, 4]);
-    const result = [7, 0, 8];
-    assert.deepEqual(addTwoNumbers(l1, l2), result);
-  });
+  const tests = [{
+    l1: [2, 4, 3],
+    l2: [5, 6, 4],
+    result: [7, 0, 8]
+  }, {
+    l1: [5, 6, 3],
+    l2: [8, 4, 2],
+    result: [3, 1, 6]
+  }, {
+    l1: [7, 5, 9, 4, 6],
+    l2: [8, 4],
+    result: [5, 0, 0, 5, 6]
+  }];
 
-  it('(5 -> 6 -> 3 ) + (8 -> 4 -> 2) = (3 -> 1 -> 6)', () => {
-    const l1 = makeLinkNodes([5, 6, 3]);
-    const l2 = makeLinkNodes([8, 4, 2]);
-    const result = [3, 1, 6];
-    assert.deepEqual(addTwoNumbers(l1, l2), result);
-  });
-
-  it('(7 -> 5 -> 9 -> 4 -> 6) + (8 -> 4) = (5 -> 0 -> 0 -> 5 -> 6)', () => {
-    const l1 = makeLinkNodes([7, 5, 9, 4, 6]);
-    const l2 = makeLinkNodes([8, 4]);
-    const result = [5, 0, 0, 5, 6];
-    assert.deepEqual(addTwoNumbers(l1, l2), result);
+  _.forEach(tests, ({ l1, l2, result }) => {
+    it(`[${l1}] + [${l2}] = [${result}]`, () => {
+      l1 = makeLinkNodes(l1);
+      l2 = makeLinkNodes(l2);
+      assert.deepEqual(addTwoNumbers(l1, l2), result);
+    });
   });
 });

70dff364af8438d98e714134b94fd2e14e437456
Suguru Motegi <suguru.motegi@gmail.com>
2016-12-27 20:58:39 -0800

    add Roman to Integer

---
+++ algorithms/Roman to Integer/index.js
@@ -0,0 +1,57 @@
+'use strict';
+
+module.exports = romanToInt;
+
+const hash = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000
+};
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+function romanToInt(s) {
+  const array = s.split('');
+  let l = array.length;
+  let sum = 0;
+  let prev = 0;
+  while (l--) {
+    const c = array[l];
+    const n = hash[c];
+    if (prev > n) {
+      sum -= n;
+    } else {
+      sum += n;
+    }
+    prev = n;
+  }
+  return sum;
+}
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+function romanToInt2(s) {
+  let l = s.length;
+  let sum = 0;
+  let prev = 0;
+  while (l--) {
+    const c = s[l];
+    const n = hash[c];
+    if (prev > n) {
+      sum -= n;
+    } else {
+      sum += n;
+    }
+    prev = n;
+  }
+  return sum;
+}
+

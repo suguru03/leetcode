f323ec88d6f9adbea2b57c499e5c71e22f92a86c
Suguru Motegi <suguru.motegi@gmail.com>
2017-01-08 16:33:27 -0800

    update Remove Duplicates from Sorted Array

--- algorithms/Remove Duplicates from Sorted Array/index.js
+++ algorithms/Remove Duplicates from Sorted Array/index.js
@@ -1,6 +1,11 @@
 'use strict';
 
-module.exports = { removeDuplicates, removeDuplicates2, removeDuplicates3 };
+module.exports = {
+  removeDuplicates,
+  removeDuplicates2,
+  removeDuplicates3,
+  removeDuplicates4
+};
 
 /**
  * @param {number[]} nums
@@ -49,10 +54,30 @@ function removeDuplicates3(nums) {
   while (l--) {
     const n = nums[l];
     if (prev !== n) {
+      prev = n;
       array.push(n);
     }
   }
   nums.length = 0;
   Array.prototype.push.apply(nums, array);
+  return nums.length;
+}
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function removeDuplicates4(nums) {
+  let l = nums.length;
+  let prev;
+  const array = [0, l];
+  while (l--) {
+    const n = nums[l];
+    if (prev !== n) {
+      prev = n;
+      array.push(n);
+    }
+  }
+  Array.prototype.splice.apply(nums, array);
   return nums.length;
 }

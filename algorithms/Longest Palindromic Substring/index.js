ad400d4a57a33b89e43fc4c26d0285661544daa8
Suguru Motegi <suguru.motegi@gmail.com>
2017-01-12 21:20:38 -0800

    add Longest Palindromic Substring

---
+++ algorithms/Longest Palindromic Substring/index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = { longestPalindrome };
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+function longestPalindrome(s) {
+  const ds = `#${s.split('').join('#')}#`;
+  const n = ds.length;
+  const rad = Array(n);
+  for (let i = 0, j = 0; i < n;) {
+    let li, ri;
+    while (0 <=  (li = i - j) && (ri = i + j) < n && ds[li] === ds[ri]) {
+      j++;
+    }
+    rad[i] = j;
+    let k = 1;
+    for (; 0 <= i - k && i + k < n && rad[i - k] < j - k; k++) {
+      rad[i + k] = rad[i - k];
+    }
+    i += k;
+    j = Math.max(j - k, 0);
+  }
+  const max = Math.max.apply(null, rad);
+  for (let i = 0; i < n; i++) {
+    if (rad[i] !== max) {
+      continue;
+    }
+    const str = ds.slice(i - max + 1, i + max);
+    return str.replace(/#/g, '');
+  }
+}
---
+++ algorithms/Longest Palindromic Substring/test.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const assert = require('assert');
+const _ = require('lodash');
+const { longestPalindrome } = require('./');
+
+describe('#longestPalindrome', () => {
+
+  const tests = [{
+    s: 'babad',
+    result: 'bab'
+  }, {
+    s: 'cbbd',
+    result: 'bb'
+  }, {
+    s: 'abb',
+    result: 'bb'
+  }, {
+    s: 'abcdefedcba',
+    result: 'abcdefedcba'
+  }, {
+    s: 'abcdeffedcba',
+    result: 'abcdeffedcba'
+  }, {
+    s: 'abcdefgffffffffiiiiiiihh',
+    result: 'ffffffff'
+  }, {
+    s: 'babcdeffedafadfacbaaffajioefeawjfea;awieojwa;efaehf;ffoajfewfo;iaeji;ofjaefi;aejalgehekjafjfjkdfkdjkfjjjffffjjjfffjf',
+    result: 'fjjjffffjjjf'
+  }, {
+    s: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+    result: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+  }];
+
+  _.forEach(tests, ({ s, result }) => {
+    it(`${s} -> ${result}`, () => {
+      assert.strictEqual(longestPalindrome(s), result);
+    });
+  });
+});

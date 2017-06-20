a23f13bf15e314102c489db3bec4ccb311c9690d
Suguru Motegi <suguru.motegi@gmail.com>
2017-06-19 17:59:13 -0700

    add 021.Merge Two Sorted Lists

---
+++ algorithms/021.Merge Two Sorted Lists/Solution.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+
+	final int MAX = Integer.MAX_VALUE;
+
+	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+		ListNode result = new ListNode(0);
+		ListNode l = result;
+		while (l1 != null && l2 != null) {
+			int v1 = l1.val;
+			int v2 = l2.val;
+			if (v1 < v2) {
+				l.next = new ListNode(v1);
+				l1 = l1.next;
+			} else {
+				l.next = new ListNode(v2);
+				l2 = l2.next;
+			}
+			l = l.next;
+		}
+		l.next = l1 != null ? l1 : l2;
+		return result.next;
+	}
+}

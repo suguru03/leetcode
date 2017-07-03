6efeb21f2f1d9fb6d0ca875eb7bf94e259fc1d32
Suguru Motegi <suguru.motegi@gmail.com>
2017-07-02 23:23:46 -0700

    update 053.Maximum Subarray

--- algorithms/053.Maximum Subarray/Solution.java
+++ algorithms/053.Maximum Subarray/Solution.java
@@ -1,25 +1,16 @@
 public class Solution {
 
-  final long MAX = Long.MAX_VALUE;
-  final long MIN = Long.MIN_VALUE;
-
   public int maxSubArray(int[] nums) {
-    long sum = 0;
-    long min = MAX;
-    long max = MIN;
-    long result = MIN;
+    int sum = 0;
+    int max = Integer.MIN_VALUE;
     for (int i = 0; i < nums.length; i++) {
       int n = nums[i];
       sum += n;
-      min = Math.min(min, sum);
       max = Math.max(max, sum);
-      result = Math.max(result, sum);
-      result = Math.max(result, n);
-      long diff = sum - min;
-      if (diff != 0) {
-        result = Math.max(result, diff);
+      if (sum < 0) {
+        sum = 0;
       }
     }
-    return (int)result;
+    return max;
   }
 }
--- algorithms/053.Maximum Subarray/index.js
+++ algorithms/053.Maximum Subarray/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = { maxSubArray };
+module.exports = { maxSubArray, maxSubArray2 };
 
 /**
  * @param {number[]} nums
@@ -10,18 +10,34 @@ function maxSubArray(nums) {
   let sum = 0;
   let min = Infinity;
   let max = -Infinity;
-  let result = -Infinity;
   let i = -1;
   while (++i < nums.length) {
     const n = nums[i];
     sum += n;
     min = Math.min(min, sum);
-    max = Math.max(max, sum);
-    result = Math.max(result, sum, n);
+    max = Math.max(max, sum, n);
     const diff = sum - min;
     if (diff !== 0) {
-      result = Math.max(result, diff);
+      max = Math.max(max, diff);
     }
   }
-  return result;
+  return max;
+}
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function maxSubArray2(nums) {
+  let sum = 0;
+  let max = -Infinity;
+  let i = -1;
+  while (++i < nums.length) {
+    sum += nums[i];
+    max = Math.max(max, sum);
+    if (sum < 0) {
+      sum = 0;
+    }
+  }
+  return max;
 }

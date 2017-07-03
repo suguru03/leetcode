ea3f19be306ade8c9ba4d27e5c7bd9512a3df3d3
Suguru Motegi <suguru.motegi@gmail.com>
2017-07-02 23:02:58 -0700

    update 104.Maximum Depth of Binary Tree

--- algorithms/104.Maximum Depth of Binary Tree/Solution.java
+++ algorithms/104.Maximum Depth of Binary Tree/Solution.java
@@ -9,10 +9,6 @@
  */
 public class Solution {
   public int maxDepth(TreeNode root) {
-    return this.getDepth(root);
-  }
-
-  private int getDepth(TreeNode node) {
-    return node == null ? 0 : 1 + Math.max(this.getDepth(node.left), this.getDepth(node.righ));
+    return root == null ? 0 : 1 + Math.max(this.getDepth(root.left), this.getDepth(root.righ));
   }
 }

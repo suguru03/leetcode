53398a14c36dacd24a5c121390ed8f3d51ac7b3d
Suguru Motegi <suguru.motegi@gmail.com>
2017-08-14 09:12:23 -0700

    update sql queries of 176 and 181

--- algorithms/176.Second Highest Salary/query.sql
+++ algorithms/176.Second Highest Salary/query.sql
@@ -1,2 +1,3 @@
 -- Write your MySQL query statement below
 SELECT MAX(Salary) SecondHighestSalary FROM Employee WHERE Salary != (SELECT MAX(Salary) FROM Employee) ORDER BY Salary DESC;
+-- SELECT DISTINCT Salary SecondHighestSalary FROM Employee ORDER BY Salary DESC LIMIT 1 OFFSET 1

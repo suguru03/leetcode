bdae964ac86ddc330d7d18bff11dff8c1fd1b131
Suguru Motegi <suguru.motegi@gmail.com>
2017-09-08 21:24:36 -0700

    add 232.Implement Queue using Stacks

---
+++ algorithms/232.Implement Queue using Stacks/index.js
@@ -0,0 +1,49 @@
+'use strict';
+
+class Data {
+
+  constructor(val, head) {
+    this.val = val;
+    this.head = head;
+    this.tail = null;
+    if (head) {
+      head.tail = this;
+    }
+  }
+}
+
+class MyQueue {
+
+  constructor() {
+    this._head = null;
+    this._tail = null;
+  }
+
+  push(x) {
+    const data = new Data(x, this._tail);
+    if (!this._head) {
+      this._head = data;
+    }
+    this._tail = data;
+  }
+
+  pop() {
+    const { _head } = this;
+    if (!_head) {
+      return null;
+    }
+    this._head = _head.tail;
+    return _head.val;
+  }
+
+  peek() {
+    return this._head ? this._head.val : null;
+  }
+
+  empty() {
+    return !this._head;
+  }
+}
+
+module.exports = { MyQueue };
+
---
+++ algorithms/232.Implement Queue using Stacks/test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const assert = require('assert');
+const { MyQueue } = require('./');
+
+describe('#MyQueue', () => {
+
+  it('should work', () => {
+    const queue = new MyQueue();
+    assert.strictEqual(queue.empty(), true);
+    assert.strictEqual(queue.push(1), undefined);
+    assert.strictEqual(queue.push(2), undefined);
+    assert.strictEqual(queue.peek(), 1);
+  });
+});

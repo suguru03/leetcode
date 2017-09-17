da232e4df6fb6c21540eaf353f3ef0d74cf23454
Suguru Motegi <suguru.motegi@gmail.com>
2017-09-16 20:36:51 -0700

    add 677.Map Sum Pairs

---
+++ algorithms/677.Map Sum Pairs/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * Initialize your data structure here.
+ */
+class MapSum {
+
+  constructor() {
+    this._map = {};
+  }
+
+  insert(key, val) {
+    for (let i = 1; i <= key.length; i++) {
+      const s = key.substring(0, i);
+      this._map[s] = this._map[s] || {};
+      this._map[s][key] = val;
+    }
+  }
+
+  sum(prefix) {
+    const obj = this._map[prefix] || {};
+    return Object.entries(obj).reduce((result, [, value]) => result + value, 0);
+  }
+}
+
+module.exports = { MapSum };
---
+++ algorithms/677.Map Sum Pairs/test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const assert = require('assert');
+const { MapSum } = require('./');
+
+describe('#MapSum', () => {
+
+  it('should work', () => {
+    const mapSum = new MapSum();
+    mapSum.insert('apple', 3);
+    assert.strictEqual(mapSum.sum('ap'), 3);
+    mapSum.insert('app', 2);
+    assert.strictEqual(mapSum.sum('ap'), 5);
+    assert.strictEqual(mapSum.sum('apple'), 3);
+  });
+});

c4a90af39339caca25b785eebf6e33847c46181c
Suguru Motegi <suguru.motegi@gmail.com>
2017-10-31 23:50:27 -0700

    add 532.K-diff Pairs in an Array

---
+++ algorithms/532.K-diff Pairs in an Array/index.js
@@ -0,0 +1,65 @@
+'use strict';
+
+module.exports = { findPairs, findPairs2 };
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+function findPairs(nums, k) {
+  nums.sort((n1, n2) => n1 - n2);
+  let right = 0;
+  let left = 1;
+  const hash = {};
+  const l = nums.length;
+  while (left < l) {
+    const nl = nums[left];
+    const nr = nums[right];
+    const diff = nl - nr;
+    if (diff < k) {
+      left++;
+    } else if (diff === k) {
+      hash[`${nl}:${nr}`] = 1;
+      right++;
+      left++;
+    } else {
+      right++;
+    }
+    if (right === left) {
+      left++;
+    }
+  }
+  return Object.keys(hash).length;
+}
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+function findPairs2(nums, k) {
+  if (k < 0) {
+    return 0;
+  }
+  nums.sort((n1, n2) => n1 - n2);
+  const l = nums.length;
+  const hash = {};
+  for (let i = 0; i < l; i++) {
+    const n = nums[i];
+    hash[n] = ++hash[n] || 1;
+  }
+  let result = 0;
+  let prev;
+  for (let i = 0; i < l; i++) {
+    const n = nums[i];
+    if (n === prev) {
+      continue;
+    }
+    prev = n;
+    if (k === 0 && hash[n] >= 2 || k !== 0 && hash[n - k] >= 1) {
+      result++;
+    }
+  }
+  return result;
+}

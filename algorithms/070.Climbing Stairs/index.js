4f1c7fe3c751e68c24f310bbcba8d2b5d3c78868
Suguru Motegi <suguru.motegi@gmail.com>
2018-01-14 23:19:48 -0800

    add 070.Climbing Stairs

--- algorithms/070.Climbing Stairs/index.js
+++ algorithms/070.Climbing Stairs/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = { climbStairs };
+module.exports = { climbStairs, climbStairs2};
 
 const map = {};
 const steps = [1, 2];
@@ -9,10 +9,6 @@ const steps = [1, 2];
  * @return {number}
  */
 function climbStairs(n) {
-  return getPatterns(n);
-}
-
-function getPatterns(n) {
   let sum = map[n] || 0;
   if (sum) {
     return sum;
@@ -22,8 +18,17 @@ function getPatterns(n) {
     if (rest < 0) {
       continue;
     }
-    sum += !rest ? 1 : getPatterns(rest);
+    sum += !rest ? 1 : climbStairs(rest);
   }
   map[n] = sum;
   return sum;
+}
+
+function climbStairs2(n) {
+  const steps = [0, 1, 2];
+  steps.length = n + 1;
+  for (let i = 3; i <= n; i++) {
+    steps[i] = steps[i - 1] + steps[i - 2];
+  }
+  return steps[n];
 }

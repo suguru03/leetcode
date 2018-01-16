37606625422131d127a75c17477ec3b0605c8e87
Suguru Motegi <suguru.motegi@gmail.com>
2018-01-16 15:15:56 -0800

    add prettier settings

--- build.js
+++ build.js
@@ -6,7 +6,10 @@ const path = require('path');
 const _ = require('lodash');
 
 const basepath = path.resolve(__dirname, 'algorithms');
-const base = fs.readFileSync(path.resolve(__dirname, 'template', 'README.md'), 'utf8');
+const base = fs.readFileSync(
+  path.resolve(__dirname, 'template', 'README.md'),
+  'utf8',
+);
 
 const re = /^(?!\d)/;
 const baseurl = 'https://github.com/suguru03/leetcode/tree/master/algorithms/';
@@ -15,7 +18,7 @@ const solutionMap = {
   java: 'Solution.java',
   ruby: 'solution.rb',
   sql: 'query.sql',
-  bash: 'solution.bash'
+  bash: 'solution.bash',
 };
 const tasks = _.chain(fs.readdirSync(basepath))
   .reject(dirname => re.test(dirname))
@@ -23,12 +26,18 @@ const tasks = _.chain(fs.readdirSync(basepath))
     const [num, name] = dirname.split('.');
     const url = `${baseurl}${dirname.replace(/\s/g, '%20')}`;
     const filenames = fs.readdirSync(path.resolve(basepath, dirname));
-    const map = _.transform(filenames, (result, name) => result[name] = name, {});
+    const map = _.transform(
+      filenames,
+      (result, name) => (result[name] = name),
+      {},
+    );
     const filepathMap = _.chain(solutionMap)
       .mapValues(name => map[name] && `${url}/${name}`)
       .omitBy(_.isUndefined)
       .value();
-    return !_.isEmpty(filepathMap) && Object.assign({ num, name, url }, filepathMap);
+    return (
+      !_.isEmpty(filepathMap) && Object.assign({ num, name, url }, filepathMap)
+    );
   })
   .filter()
   .value();
@@ -37,14 +46,20 @@ if (_.isEmpty(tasks)) {
   return;
 }
 
-const readme = _.reduce(tasks, (result, obj) => {
-  const { num, name, url } = obj;
-  const solution = _.chain(obj)
-    .pick(_.keys(solutionMap))
-    .map((url, key) => `[${key}](${url})`)
-    .join(', ')
-    .value();
-  return result + `\n|${num}|[${name}](${url})|${solution}|`;
-}, base + '\n## Links\n|#|Title|Solution|\n|---|---|---|');
+console.log('building README...');
+
+const readme = _.reduce(
+  tasks,
+  (result, obj) => {
+    const { num, name, url } = obj;
+    const solution = _.chain(obj)
+      .pick(_.keys(solutionMap))
+      .map((url, key) => `[${key}](${url})`)
+      .join(', ')
+      .value();
+    return result + `\n|${num}|[${name}](${url})|${solution}|`;
+  },
+  base + '\n## Links\n|#|Title|Solution|\n|---|---|---|',
+);
 
 fs.writeFileSync(path.resolve(__dirname, 'README.md'), readme, 'utf8');

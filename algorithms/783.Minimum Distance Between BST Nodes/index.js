bf03061cddc9f1e518f1f01ba932f1090a3873aa
Suguru Motegi <suguru.motegi@gmail.com>
2019-01-13 13:22:35 -0800

    update 783.Minimum Distance Between BST Nodes

--- algorithms/783.Minimum Distance Between BST Nodes/index.js
+++ algorithms/783.Minimum Distance Between BST Nodes/index.js
@@ -14,25 +14,19 @@ module.exports = { minDiffInBST };
  * @return {number}
  */
 function minDiffInBST(root) {
-  const map = {};
-  resolve(root);
-  const keys = Object.keys(map).sort((n1, n2) => n1 - n2);
-  let prev = -Infinity;
   let min = Infinity;
-  for (let i = 0; i < keys.length; i++) {
-    const key = keys[i];
-    min = Math.min(min, Math.abs(key - prev));
-    prev = key;
-  }
+  let prev = -Infinity;
+  dfs(root);
   return min;
 
-  function resolve(node) {
+  function dfs(node) {
     if (!node) {
       return;
     }
-    const { val, right, left } = node;
-    map[val] = ++map[val] || 1;
-    resolve(right);
-    resolve(left);
+    const { left, val, right } = node;
+    dfs(left);
+    min = Math.min(min, val - prev);
+    prev = val;
+    dfs(right);
   }
 }

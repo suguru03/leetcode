ec5cb834fb20b756dc5cc451993f67baa3f720d5
Suguru Motegi <suguru.motegi@gmail.com>
2019-01-24 22:49:12 -0800

    update 237.Delete Node in a Linked List

--- algorithms/237.Delete Node in a Linked List/index.js
+++ algorithms/237.Delete Node in a Linked List/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = { deleteNode };
+module.exports = { deleteNode, deleteNode2 };
 
 /**
  * Definition for singly-linked list.
@@ -16,4 +16,12 @@ module.exports = { deleteNode };
 function deleteNode(node) {
   node.val = node.next.val;
   node.next = node.next.next;
+}
+
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+function deleteNode2(node) {
+  Object.assign(node, node.next);
 }

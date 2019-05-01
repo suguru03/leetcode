326017b923e9be32b170b18524dfc08cd2f02c0a
Suguru Motegi <suguru.motegi@gmail.com>
2019-05-01 15:24:34 -0700

    update 001.Two Sum

--- algorithms/001.Two Sum/index.js
+++ algorithms/001.Two Sum/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = { twoSum };
+module.exports = { twoSum, twoSum2 };
 
 /**
  * @param {number[]} nums
@@ -18,5 +18,21 @@ function twoSum(nums, target) {
       return [l, i];
     }
     hash[n] = l;
+  }
+}
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+function twoSum2(nums, target) {
+  const hash = new Map();
+  for (const [i, n] of nums.entries()) {
+    const diff = target - n;
+    if (hash.has(diff)) {
+      return [hash.get(diff), i];
+    }
+    hash.set(n, i);
   }
 }

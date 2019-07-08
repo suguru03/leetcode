2ccaee2b41b93396967d9a526670c17a0d59ee5b
Suguru Motegi <suguru.motegi@gmail.com>
2019-07-08 12:02:18 -0700

    update 344.Reverse String

--- algorithms/344.Reverse String/index.js
+++ algorithms/344.Reverse String/index.js
@@ -8,6 +8,14 @@ module.exports = { reverseString, reverseString2 };
  */
 function reverseString(s) {
   const l = s.length;
+  for (let i = 0; i < l / 2; i++) {
+    const j = l - i - 1;
+    [s[i], s[j]] = [s[j], s[i]];
+  }
+}
+
+function reverseString2(s) {
+  const l = s.length;
   reverse(0);
 
   function reverse(i) {
@@ -19,47 +27,3 @@ function reverseString(s) {
     reverse(++i);
   }
 }
-
-function reverseString2(s) {
-  let i = -1;
-  const l = s.length;
-  while (++i < ((l / 2) | 0)) {
-    const j = l - i - 1;
-    [s[i], s[j]] = [s[j], s[i]];
-  }
-}
-
-// /**
-//  * @param {string} s
-//  * @return {string}
-//  */
-// function reverseString(s) {
-//   let l = s.length;
-//   let result = '';
-//   while (l--) {
-//     result += s[l];
-//   }
-//   return result;
-// }
-//
-// /**
-//  * @param {string} s
-//  * @return {string}
-//  */
-// function reverseString2(s) {
-//   let l = s.length;
-//   if (l <= 1) {
-//     return s;
-//   }
-//   const even = l % 2 === 0;
-//   if (even) {
-//     s += 'a';
-//   }
-//   l = (l / 2) | 0;
-//   let middle = l;
-//   let result = s[l];
-//   while (l--) {
-//     result = `${s[2 * middle - l]}${result}${s[l]}`;
-//   }
-//   return even ? result.substr(1, result.length) : result;
-// }

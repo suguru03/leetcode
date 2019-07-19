74a6394085a45f89a5bda7677a06ab500e7ba672
Suguru Motegi <suguru.motegi@gmail.com>
2019-07-19 16:04:28 -0700

    update 599.Minimum Index Sum of Two Lists

--- algorithms/599.Minimum Index Sum of Two Lists/index.js
+++ algorithms/599.Minimum Index Sum of Two Lists/index.js
@@ -12,18 +12,18 @@ function findRestaurant(list1, list2) {
   for (const [index, item] of list1.entries()) {
     map.set(item, index);
   }
-  let max = Infinity;
+  let min = Infinity;
   const result = [];
   for (const [index, item] of list2.entries()) {
     if (!map.has(item)) {
       continue;
     }
     const sum = map.get(item) + index;
-    if (sum > max) {
+    if (sum > min) {
       continue;
     }
-    if (sum < max) {
-      max = sum;
+    if (sum < min) {
+      min = sum;
       result.length = 0;
     }
     result.push(item);

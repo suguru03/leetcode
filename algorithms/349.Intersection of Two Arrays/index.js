9dd2989a3b8c346e2333509d6c90f22dad3b6d26
Suguru Motegi <suguru.motegi@gmail.com>
2019-08-11 14:37:50 -0700

    update 349.Intersection of Two Arrays

--- algorithms/349.Intersection of Two Arrays/index.js
+++ algorithms/349.Intersection of Two Arrays/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = { intersection, intersection2 };
+module.exports = { intersection, intersection2, intersection3, intersection4 };
 
 /**
  * @param {number[]} nums1
@@ -39,4 +39,50 @@ function intersection2(nums1, nums2) {
     }
   }
   return Array.from(result);
+}
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+function intersection3(nums1, nums2) {
+  const s1 = new Set(nums1);
+  const s2 = new Set(nums2);
+  const res = [];
+  for (const n of s2) {
+    if (s1.has(n)) {
+      res.push(n);
+    }
+  }
+  return res;
+}
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+function intersection4(nums1, nums2) {
+  nums1.sort((n1, n2) => n1 - n2);
+  nums2.sort((n1, n2) => n1 - n2);
+  let i = 0;
+  let j = 0;
+  const res = new Set();
+  while (i < nums1.length && j < nums2.length) {
+    const n1 = nums1[i];
+    const n2 = nums2[j];
+    if (n1 === n2) {
+      res.add(n1);
+      i++;
+      j++;
+      continue;
+    }
+    if (n1 < n2) {
+      i++;
+    } else {
+      j++;
+    }
+  }
+  return Array.from(res);
 }

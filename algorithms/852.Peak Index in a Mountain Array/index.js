391697628828d2e651931001e7693d5e79bc9eca
Suguru Motegi <suguru.motegi@gmail.com>
2019-08-12 12:24:18 -0700

    update 852.Peak Index in a Mountain Array

--- algorithms/852.Peak Index in a Mountain Array/index.js
+++ algorithms/852.Peak Index in a Mountain Array/index.js
@@ -7,31 +7,43 @@ module.exports = { peakIndexInMountainArray, peakIndexInMountainArray2 };
  * @return {number}
  */
 function peakIndexInMountainArray(A) {
-  let [prev] = A;
-  for (let i = 1; i < A.length; i++) {
-    const n = A[i];
-    if (n < prev) {
-      return i - 1;
+  let l = 0;
+  let r = A.length - 1;
+  while (l < r) {
+    const m = ((l + r) / 2) | 0;
+    if (A[m] < A[m + 1]) {
+      l = m + 1;
+    } else {
+      r = m;
     }
-    prev = n;
   }
-  return -1;
+  return l;
 }
 
 /**
+ * @see https://leetcode.com/problems/peak-index-in-a-mountain-array/discuss/139848/C%2B%2BJavaPython-Better-than-Binary-Search
  * @param {number[]} A
  * @return {number}
  */
 function peakIndexInMountainArray2(A) {
-  let left = 0;
-  let right = A.length - 1;
-  while (left < right) {
-    const middle = ((left + right) / 2) | 0;
-    if (A[middle] < A[middle + 1]) {
-      left = middle + 1;
+  let l = 0;
+  let r = A.length - 1;
+  let x1 = gold1(l, r);
+  let x2 = gold2(l, r);
+  while (x1 < x2) {
+    if (A[x1] < A[x2]) {
+      [l, x1, x2] = [x1, x2, gold1(x2, r)];
     } else {
-      right = middle;
+      [r, x2, x1] = [x2, x1, gold2(l, x1)];
     }
   }
-  return left;
+  return ((l + r) / 2) | 0;
+}
+
+function gold1(l, r) {
+  return l + Math.round((r - l) * 0.382);
+}
+
+function gold2(l, r) {
+  return l + Math.round((r - l) * 0.618);
 }

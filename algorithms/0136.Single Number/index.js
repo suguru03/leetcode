9131bedb93e97c7452f1ef9607625cd408736be4
Suguru Motegi <suguru.motegi@gmail.com>
2019-08-19 21:21:29 -0700

    add 0136.Single Number

--- algorithms/0136.Single Number/index.js
+++ algorithms/0136.Single Number/index.js
@@ -1,16 +1,25 @@
 'use strict';
 
-module.exports = { singleNumber };
+module.exports = { singleNumber, singleNumber2 };
 
 /**
  * @param {number[]} nums
  * @return {number}
  */
 function singleNumber(nums) {
-  let l = nums.length;
-  let result = 0;
-  while (l--) {
-    result ^= nums[l];
+  nums.sort((n1, n2) => n1 - n2);
+  for (let i = 0; i < nums.length; i++) {
+    const n = nums[i];
+    if (n !== nums[i - 1] && n !== nums[i + 1]) {
+      return n;
+    }
   }
-  return result;
+}
+
+function singleNumber2(nums) {
+  let bit = 0;
+  for (const n of nums) {
+    bit ^= n;
+  }
+  return bit;
 }

58a3c145496afff68423e5a2864252d296e675a7
Suguru Motegi <suguru.motegi@gmail.com>
2019-08-22 12:19:35 -0700

    update 0231.Power of Two

--- algorithms/0231.Power of Two/index.js
+++ algorithms/0231.Power of Two/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = { isPowerOfTwo, isPowerOfTwo2 };
+module.exports = { isPowerOfTwo, isPowerOfTwo2, isPowerOfTwo3 };
 
 /**
  * @param {number} n
@@ -17,9 +17,18 @@ function isPowerOfTwo(n) {
 }
 
 /**
+ * @see https://leetcode.com/problems/power-of-two/discuss/63974/Using-nand(n-1)-trick
  * @param {number} n
  * @return {boolean}
  */
 function isPowerOfTwo2(n) {
   return n > 0 && (n & (n - 1)) === 0;
+}
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+function isPowerOfTwo3(n) {
+  return (Math.log10(n) / Math.log10(2)) % 1 === 0;
 }

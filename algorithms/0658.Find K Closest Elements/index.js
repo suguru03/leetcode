3ff52057b5170f8bf0a9d463f4ec8a0e69d7781b
Suguru Motegi <suguru.motegi@gmail.com>
2019-09-22 18:01:09 -0700

    Add 0658.Find K Closest Elements

--- algorithms/0658.Find K Closest Elements/index.js
+++ algorithms/0658.Find K Closest Elements/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = { findClosestElements, findClosestElements2 };
+module.exports = { findClosestElements, findClosestElements2, findClosestElements3 };
 
 /**
  * @param {number[]} arr
@@ -47,4 +47,47 @@ function findClosestElements2(arr, k, x) {
     const a2 = Math.abs(x - n2);
     return a1 === a2 ? n1 - n2 : a1 - a2;
   }
+}
+
+function findClosestElements3(arr, k, x) {
+  let low = 0;
+  let high = arr.length - 1;
+  while (low + 1 < high) {
+    const mid = ((low + high) / 2) | 0;
+    if (arr[mid] < x) {
+      low = mid;
+    } else {
+      high = mid;
+    }
+  }
+  let left = low;
+  let right = left;
+  let diff = 0;
+  k--;
+  while (k) {
+    while (k && left > 0 && x - arr[left - 1] === diff) {
+      k--;
+      left--;
+    }
+    while (k && right < arr.length && arr[right + 1] - x === diff) {
+      k--;
+      right++;
+    }
+    diff++;
+  }
+  return arr.slice(left, right + 1);
+}
+
+function findClosestElements(arr, k, x) {
+  let left = 0;
+  let right = arr.length - k;
+  while (left < right) {
+    const mid = ((left + right) / 2) | 0;
+    if (x - arr[mid] > arr[mid + k] - x) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+  return arr.slice(left, left + k);
 }
--- algorithms/0658.Find K Closest Elements/test.js
+++ algorithms/0658.Find K Closest Elements/test.js
@@ -36,8 +36,31 @@ describe('#findClosestElements', () => {
       x: 5,
       result: [2, 3, 4, 5],
     },
+    {
+      arr: [0, 1, 1, 1, 2, 3, 6, 7, 8, 9],
+      k: 9,
+      x: 4,
+      result: [0, 1, 1, 1, 2, 3, 6, 7, 8],
+    },
+    {
+      arr: [0, 0, 0, 1, 3, 5, 6, 7, 8, 8],
+      k: 2,
+      x: 2,
+      result: [1, 3],
+    },
+    {
+      arr: [0, 0, 1, 2, 3, 3, 4, 7, 7, 8],
+      k: 3,
+      x: 5,
+      result: [3, 3, 4],
+    },
+    {
+      arr: [1, 1, 2, 3, 3, 3, 4, 6, 8, 8],
+      k: 6,
+      x: 1,
+      result: [1, 1, 2, 3, 3, 3],
+    },
   ];
-
   _.forEach(tests, ({ arr, k, x, result }) => {
     it(`[${arr}], k: ${k}, x: ${x} -> [${result}]`, () => {
       assert.deepEqual(findClosestElements(arr, k, x), result);

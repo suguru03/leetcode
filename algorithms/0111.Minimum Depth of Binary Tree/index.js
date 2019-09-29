f5ea206e329b87d6cf43cc5dfde9f5fd03b1a55c
Suguru Motegi <suguru.motegi@gmail.com>
2019-09-29 16:38:44 -0700

    Update 0111.Minimum Depth of Binary Tree

--- algorithms/0111.Minimum Depth of Binary Tree/index.js
+++ algorithms/0111.Minimum Depth of Binary Tree/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = { minDepth, minDepth2, minDepth3 };
+module.exports = { minDepth, minDepth2, minDepth3, minDepth4 };
 
 /**
  * Definition for a binary tree node.
@@ -66,5 +66,56 @@ function minDepth3(root) {
       }
     }
     depth++;
+  }
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+function minDepth3(root) {
+  if (!root) {
+    return 0;
+  }
+  const queue = [root];
+  let depth = 1;
+  while (true) {
+    let l = queue.length;
+    while (l--) {
+      const { left, right } = queue.shift();
+      if (left === right) {
+        return depth;
+      }
+      if (left) {
+        queue.push(left);
+      }
+      if (right) {
+        queue.push(right);
+      }
+    }
+    depth++;
+  }
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+function minDepth4(root) {
+  if (!root) {
+    return 0;
+  }
+  const queue = [[root, 1]];
+  while (queue.length) {
+    const [{ left, right }, depth] = queue.shift();
+    if (left === right) {
+      return depth;
+    }
+    if (left) {
+      queue.push([left, depth + 1]);
+    }
+    if (right) {
+      queue.push([right, depth + 1]);
+    }
   }
 }

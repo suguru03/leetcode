1cb7bfbb8bad3d5235e17f5a12618e3d23147ed0
Suguru Motegi <suguru.motegi@gmail.com>
2019-11-07 12:16:38 -0800

    Update 0771.Jewels and Stones

--- algorithms/0771.Jewels and Stones/index.js
+++ algorithms/0771.Jewels and Stones/index.js
@@ -8,17 +8,6 @@ module.exports = { numJewelsInStones };
  * @return {number}
  */
 function numJewelsInStones(J, S) {
-  const map = {};
-  for (let i = 0; i < S.length; i++) {
-    const c = S[i];
-    map[c] = ++map[c] || 1;
-  }
-  let result = 0;
-  for (let i = 0; i < J.length; i++) {
-    const n = map[J[i]];
-    if (n) {
-      result += n;
-    }
-  }
-  return result;
+  const set = new Set(J);
+  return S.split('').filter(c => set.has(c)).length;
 }

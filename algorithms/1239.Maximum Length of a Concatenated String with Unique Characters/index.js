fd2ea16cfd520c6490554f558d3e74489aaa3ace
Suguru Motegi <suguru.motegi@gmail.com>
2019-11-10 00:54:34 -0800

    Update 1239.Maximum Length of a Concatenated String with Unique Characters

--- algorithms/1239.Maximum Length of a Concatenated String with Unique Characters/index.js
+++ algorithms/1239.Maximum Length of a Concatenated String with Unique Characters/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = { maxLength };
+module.exports = { maxLength, maxLength2: maxScoreWords() };
 
 /**
  * @param {string[]} arr
@@ -35,5 +35,61 @@ function maxLength(arr) {
       }
     }
     return max;
+  }
+}
+
+/**
+ * @param {string[]} words
+ * @param {character[]} letters
+ * @param {number[]} scores
+ * @return {number}
+ */
+function maxScoreWords(
+  words,
+  letters = Array.from({ length: 26 }, (_, i) => String.fromCharCode(i + 97)),
+  scores = Array.from({ length: 26 }, () => 1),
+) {
+  const countMap = {};
+  const cur = {};
+  for (const [i] of scores.entries()) {
+    const c = String.fromCharCode(i + 97);
+    cur[c] = 0;
+    countMap[c] = 0;
+  }
+  for (const c of letters) {
+    countMap[c]++;
+  }
+  const counts = words.map(word => {
+    const map = new Map();
+    for (const c of word) {
+      map.set(c, map.has(c) ? map.get(c) + 1 : 1);
+    }
+    return map;
+  });
+
+  let max = 0;
+  func(0, 0, []);
+  return max;
+
+  function func(index, score) {
+    loop: for (let i = index; i < counts.length; i++) {
+      const map = counts[i];
+      for (let [c, count] of map) {
+        if (cur[c] + count > countMap[c]) {
+          continue loop;
+        }
+      }
+      for (let [c, count] of map) {
+        cur[c] += count;
+        score += scores[c.charCodeAt(0) - 97] * count;
+      }
+      const s = func(i + 1, score);
+      max = Math.max(max, s);
+      for (let [c, count] of map) {
+        cur[c] -= count;
+        score -= scores[c.charCodeAt(0) - 97] * count;
+      }
+    }
+    return score;
   }
 }

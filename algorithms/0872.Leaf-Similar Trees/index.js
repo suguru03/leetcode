037901dd68073eb5742db2778f2fa8a10375124b
Suguru Motegi <suguru.motegi@gmail.com>
2019-11-25 23:30:47 -0800

    Update 0872.Leaf-Similar Trees

--- algorithms/0872.Leaf-Similar Trees/index.js
+++ algorithms/0872.Leaf-Similar Trees/index.js
@@ -1,12 +1,12 @@
 'use strict';
 
-module.exports = { leafSimilar };
+module.exports = { leafSimilar, leafSimilar2, leafSimilar3 };
 
 /**
  * Definition for a binary tree node.
  * function TreeNode(val) {
- *   this.val = val
- *   this.left = this.right = null
+ *     this.val = val;
+ *     this.left = this.right = null;
  * }
  */
 /**
@@ -15,21 +15,94 @@ module.exports = { leafSimilar };
  * @return {boolean}
  */
 function leafSimilar(root1, root2) {
-  const leaves1 = [];
-  const leaves2 = [];
-  find(root1, leaves1);
-  find(root2, leaves2);
+  const leaves1 = dfs(root1);
+  const leaves2 = dfs(root2);
   return leaves1.length === leaves2.length && leaves1.every((n, i) => n === leaves2[i]);
 
-  function find(node, arr) {
+  function dfs(node) {
     if (!node) {
-      return true;
+      return [];
     }
-    const ll = find(node.left, arr);
-    const lr = find(node.right, arr);
-    if (ll && lr) {
-      arr.push(node.val);
+    const { val, left, right } = node;
+    if (!left && !right) {
+      return [val];
     }
-    return false;
+    return [...dfs(left), ...dfs(right)];
   }
+}
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {boolean}
+ */
+function leafSimilar2(root1, root2) {
+  const leaves1 = dfs(root1);
+  const leaves2 = dfs(root2);
+  return leaves1.length === leaves2.length && leaves1.every((n, i) => n === leaves2[i]);
+
+  function dfs(root) {
+    const leaves = [];
+    const stack = [root];
+    while (stack.length) {
+      const node = stack.pop();
+      if (!node) {
+        continue;
+      }
+      const { val, left, right } = node;
+      if (left || right) {
+        stack.push(left, right);
+        continue;
+      }
+      leaves.push(val);
+    }
+    return leaves;
+  }
+}
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {boolean}
+ */
+function leafSimilar3(root1, root2) {
+  let prev = null;
+  const stack1 = [root1];
+  const stack2 = [root2];
+  let stack = stack1;
+  while (stack.length) {
+    const node = stack.pop();
+    if (!node) {
+      continue;
+    }
+    const { val, left, right } = node;
+    if (left || right) {
+      stack.push(left, right);
+      continue;
+    }
+    if (stack === stack1) {
+      prev = val;
+      stack = stack2;
+      continue;
+    }
+    if (prev !== val) {
+      return false;
+    }
+    stack = stack1;
+  }
+  return stack1.length === stack2.filter(n => n !== null).length;
 }
--- algorithms/0872.Leaf-Similar Trees/test.js
+++ algorithms/0872.Leaf-Similar Trees/test.js
@@ -12,6 +12,11 @@ describe('#leafSimilar', () => {
       root2: [3, 5, 1, 6, 7, 4, 2, null, null, null, null, null, null, 9, 8],
       result: true,
     },
+    {
+      root1: [41, 62, null, 66, null, null, 21, 96, null, 70, 74],
+      root2: [55, null, 84, null, 29, 116, null, 7, 74, null, 70],
+      result: true,
+    },
   ];
   makeTestTreeNodes(tests, 'root1');
   makeTestTreeNodes(tests, 'root2');

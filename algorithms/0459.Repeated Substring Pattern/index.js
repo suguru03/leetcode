e789ca745f1f067ecc53ca3a6ae4e09237af0f64
Suguru Motegi <suguru.motegi@gmail.com>
2019-12-02 12:47:41 -0800

    Update 0459.Repeated Substring Pattern

--- algorithms/0459.Repeated Substring Pattern/index.js
+++ algorithms/0459.Repeated Substring Pattern/index.js
@@ -1,26 +1,25 @@
 'use strict';
 
-module.exports = { repeatedSubstringPattern };
+module.exports = { repeatedSubstringPattern, repeatedSubstringPattern2 };
 
 /**
  * @param {string} s
  * @return {boolean}
  */
 function repeatedSubstringPattern(s) {
-  const l = s.length;
-  let i = l;
-  loop: while (--i) {
-    if (l % i !== 0) {
-      continue;
-    }
-    const cur = s.slice(l - i, l);
-    const size = cur.length;
-    for (let j = 0; j < l - 1; j += size) {
-      if (s.slice(j, j + size) !== cur) {
-        continue loop;
-      }
-    }
-    return true;
-  }
-  return false;
+  const size = s.length;
+  let cur = (1 + size / 2) | 0;
+  while (--cur && s.slice(0, cur).repeat(size / cur) !== s) {}
+  return cur !== 0;
+}
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+function repeatedSubstringPattern2(s) {
+  return s
+    .repeat(2)
+    .slice(1, -1)
+    .includes(s);
 }

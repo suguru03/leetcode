664a48561a2c52f239950a494055ae2ab70130d8
Suguru Motegi <suguru.motegi@gmail.com>
2019-12-11 12:08:56 -0800

    Update 0733.Flood Fill

--- algorithms/0733.Flood Fill/index.js
+++ algorithms/0733.Flood Fill/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = { floodFill };
+module.exports = { floodFill, floodFill2 };
 
 /**
  * @param {number[][]} image
@@ -27,4 +27,29 @@ function floodFill(image, sr, sc, newColor) {
     dfs(x, y - 1);
     dfs(x, y + 1);
   }
+}
+
+/**
+ * @param {number[][]} image
+ * @param {number} sr
+ * @param {number} sc
+ * @param {number} newColor
+ * @return {number[][]}
+ */
+function floodFill2(image, sr, sc, newColor) {
+  const color = image[sr][sc];
+  if (color === newColor) {
+    return image;
+  }
+  const queue = [{ x: sc, y: sr }];
+  while (queue.length) {
+    const { x, y } = queue.shift();
+    const row = image[y];
+    if (!row || row[x] !== color) {
+      continue;
+    }
+    row[x] = newColor;
+    queue.push({ x: x - 1, y }, { x: x + 1, y }, { x, y: y - 1 }, { x, y: y + 1 });
+  }
+  return image;
 }

754f2428517b71ad16768a86c1096d6dec27d4de
Suguru Motegi <suguru.motegi@gmail.com>
2019-12-14 16:59:00 -0800

    Update 0938.Range Sum of BST

--- algorithms/0938.Range Sum of BST/index.js
+++ algorithms/0938.Range Sum of BST/index.js
@@ -15,26 +15,19 @@ module.exports = { rangeSumBST, rangeSumBST2 };
  * @param {number} R
  * @return {number}
  */
-function rangeSumBST(root, L, R) {
-  let sum = 0;
-  dfs(root);
-  return sum;
-
-  function dfs(node) {
-    if (!node) {
-      return;
-    }
-    const { left, right, val } = node;
-    if (val > L) {
-      dfs(left);
-    }
-    if (val >= L && val <= R) {
-      sum += val;
-    }
-    if (val < R) {
-      dfs(right);
-    }
+function rangeSumBST(node, L, R) {
+  if (!node) {
+    return 0;
   }
+  const { val, left, right } = node;
+  let sum = val >= L && val <= R ? val : 0;
+  if (val > L) {
+    sum += rangeSumBST(left, L, R);
+  }
+  if (val < R) {
+    sum += rangeSumBST(right, L, R);
+  }
+  return sum;
 }
 
 function rangeSumBST2(node, L, R) {

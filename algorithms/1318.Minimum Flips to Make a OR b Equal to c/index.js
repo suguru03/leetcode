f8f0bdec0fffb2c014e27383dc5469ea1e625ecd
Suguru Motegi <suguru.motegi@gmail.com>
2020-01-18 17:31:48 -0800

    Update 1318.Minimum Flips to Make a OR b Equal to c

--- algorithms/1318.Minimum Flips to Make a OR b Equal to c/index.js
+++ algorithms/1318.Minimum Flips to Make a OR b Equal to c/index.js
@@ -9,21 +9,12 @@ module.exports = { minFlips };
  * @return {number}
  */
 function minFlips(a, b, c) {
-  const as = a.toString(2);
-  const bs = b.toString(2);
-  const cs = c.toString(2);
   let count = 0;
-  const max = Math.max(as.length, bs.length, cs.length);
-  for (let i = 0; i < max; i++) {
-    const bit = ~~cs[cs.length - i - 1];
-    const a = ~~as[as.length - i - 1];
-    const b = ~~bs[bs.length - i - 1];
-    if (bit) {
-      count += a || b ? 0 : 1; // nor
-    } else {
-      count += a ? 1 : 0;
-      count += b ? 1 : 0;
-    }
+  while (a || b || c) {
+    count += c & 1 ? ((a | b) & 1) ^ 1 : (a & 1) + (b & 1);
+    a >>= 1;
+    b >>= 1;
+    c >>= 1;
   }
   return count;
 }

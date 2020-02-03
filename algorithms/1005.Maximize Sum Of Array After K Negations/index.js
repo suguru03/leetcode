ea8713827c3b85f7be605337fb3ba6ded5ddd077
Suguru Motegi <suguru.motegi@gmail.com>
2020-02-03 13:14:42 -0800

    Update 1005.Maximize Sum Of Array After K Negations

--- algorithms/1005.Maximize Sum Of Array After K Negations/index.js
+++ algorithms/1005.Maximize Sum Of Array After K Negations/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = { largestSumAfterKNegations };
+module.exports = { largestSumAfterKNegations, largestSumAfterKNegations2 };
 
 /**
  * @param {number[]} A
@@ -16,4 +16,33 @@ function largestSumAfterKNegations(A, K) {
   const index = A[i - 1] < A[i] ? i - 1 : i;
   A[index] *= (K - i) % 2 ? -1 : 1;
   return A.reduce((sum, n) => sum + n);
+}
+
+/**
+ * @param {number[]} A
+ * @param {number} K
+ * @return {number}
+ */
+function largestSumAfterKNegations2(A, K) {
+  const offset = 100;
+  const arr = Array(2 * offset + 1).fill(0);
+  for (const n of A) {
+    arr[n + offset]++;
+  }
+  let i = 0;
+  while (i < offset && K > 0) {
+    if (arr[i] === 0) {
+      i++;
+      continue;
+    }
+    K--;
+    arr[i]--;
+    arr[2 * offset - i]++;
+  }
+  if (K % 2) {
+    const index = arr.slice(offset).findIndex(count => count > 0);
+    arr[index + offset]--;
+    arr[offset - index]++;
+  }
+  return arr.reduce((sum, count, i) => sum + (i - offset) * count, 0);
 }

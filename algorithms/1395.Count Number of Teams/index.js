d7f4c010c62895bda0f95222dadd0bd197a1e6c6
Suguru Motegi <suguru.motegi@gmail.com>
2020-03-31 22:26:07 -0700

    Update 1395.Count Number of Teams

--- algorithms/1395.Count Number of Teams/index.js
+++ algorithms/1395.Count Number of Teams/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = { numTeams };
+module.exports = { numTeams, numTeams2 };
 
 /**
  * @param {number[]} rating
@@ -9,20 +9,54 @@ module.exports = { numTeams };
 function numTeams(rating) {
   let count = 0;
   for (let i = 0; i < rating.length - 2; i++) {
-    const ni = rating[i];
+    const ri = rating[i];
     for (let j = i + 1; j < rating.length - 1; j++) {
-      const nj = rating[j];
+      const rj = rating[j];
       for (let k = j + 1; k < rating.length; k++) {
-        const nk = rating[k];
-        if (ni < nj && nj < nk) {
+        const rk = rating[k];
+        if (ri < rj && rj < rk) {
           count++;
           continue;
         }
-        if (ni > nj && nj > nk) {
+        if (ri > rj && rj > rk) {
           count++;
         }
       }
     }
   }
   return count;
+}
+
+/**
+ * @see https://leetcode.com/problems/count-number-of-teams/discuss/554795/C%2B%2BJava-O(n-*-n)
+ * @param {number[]} rating
+ * @return {number}
+ */
+function numTeams2(rating) {
+  let result = 0;
+  for (let i = 1; i < rating.length - 1; i++) {
+    const ri = rating[i];
+    const less = { left: 0, right: 0 };
+    const greater = { left: 0, right: 0 };
+    for (let j = 0; j < rating.length; j++) {
+      const rj = rating[j];
+      if (ri > rj) {
+        if (j > i) {
+          less.right++;
+        } else {
+          less.left++;
+        }
+        continue;
+      }
+      if (ri < rj) {
+        if (j > i) {
+          greater.right++;
+        } else {
+          greater.left++;
+        }
+      }
+    }
+    result += less.left * greater.right + less.right * greater.left;
+  }
+  return result;
 }

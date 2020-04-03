023abd5bc22c70af8f2544ab915b9161caca2fc9
Suguru Motegi <suguru.motegi@gmail.com>
2020-04-02 23:33:37 -0700

    Update 0235.Lowest Common Ancestor of a Binary Search Tree

--- algorithms/0235.Lowest Common Ancestor of a Binary Search Tree/index.js
+++ algorithms/0235.Lowest Common Ancestor of a Binary Search Tree/index.js
@@ -10,23 +10,23 @@ module.exports = { lowestCommonAncestor };
  * }
  */
 /**
- * @param {TreeNode} root
+ * @param {TreeNode}node
  * @param {TreeNode} p
  * @param {TreeNode} q
  * @return {TreeNode}
  */
-function lowestCommonAncestor(root, p, q) {
-  if (!root) {
+function lowestCommonAncestor(node, p, q) {
+  if (!node) {
     return null;
   }
-  const vr = root.val;
+  const vn = node.val;
   const vp = p.val;
   const vq = q.val;
-  if (vr > Math.max(vp, vq)) {
-    return lowestCommonAncestor(root.left, p, q);
+  if (vn > Math.max(vp, vq)) {
+    return lowestCommonAncestor(node.left, p, q);
   }
-  if (vr < Math.min(vp, vq)) {
-    return lowestCommonAncestor(root.right, p, q);
+  if (vn < Math.min(vp, vq)) {
+    return lowestCommonAncestor(node.right, p, q);
   }
-  return root;
+  return node;
 }
--- algorithms/0235.Lowest Common Ancestor of a Binary Search Tree/test.js
+++ algorithms/0235.Lowest Common Ancestor of a Binary Search Tree/test.js
@@ -2,6 +2,7 @@
 
 const assert = require('assert');
 const _ = require('lodash');
+
 const { lowestCommonAncestor } = require('./');
 const { TreeNode, makeTestTreeNodes } = require('../util');
 

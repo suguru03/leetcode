8c5cbb5f2a5eadce20f1af1c9f9305478e3e2486
Suguru Motegi <suguru.motegi@gmail.com>
2020-05-01 21:05:45 -0700

    Add 0278.First Bad Version

---
+++ algorithms/0278.First Bad Version/solution.go
@@ -0,0 +1,22 @@
+package main
+
+/**
+ * Forward declaration of isBadVersion API.
+ * @param   version   your guess about first bad version
+ * @return 	 	      true if current version is bad
+ *			          false if current version is good
+ * func isBadVersion(version int) bool;
+ */
+
+func firstBadVersion(n int) int {
+	left, right := 1, n
+	for left < right {
+		mid := (left + right) / 2
+		if isBadVersion(mid) {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+	return left
+}

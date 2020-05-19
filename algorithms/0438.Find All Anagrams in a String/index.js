54bdc8ed0a3fc43b7b98bc220d9dd6ff77056043
Suguru Motegi <suguru.motegi@gmail.com>
2020-05-18 17:25:31 -0700

    Update 0438.Find All Anagrams in a String

--- algorithms/0438.Find All Anagrams in a String/index.js
+++ algorithms/0438.Find All Anagrams in a String/index.js
@@ -9,27 +9,25 @@ module.exports = { findAnagrams };
  */
 function findAnagrams(s, p) {
   const chars = Array(26).fill(0);
-  for (const c of p) {
-    chars[charCodeAt(c, 0)]++;
+  for (let i = 0; i < p.length; i++) {
+    chars[charCodeAt(p, i)]++;
   }
-  let left = -1;
+  let left = 0;
+  let right = 0;
   const result = [];
-  const current = Array(26).fill(0);
-  for (let right = 0; right < s.length; right++) {
-    const code = charCodeAt(s, right);
-    while (current[code] >= chars[code] && left < right) {
-      current[charCodeAt(s, ++left)]--;
-    }
-    if (current[code] < chars[code]) {
-      current[code]++;
+  while (right < s.length) {
+    if (chars[charCodeAt(s, right)] > 0) {
+      chars[charCodeAt(s, right++)]--;
+    } else {
+      chars[charCodeAt(s, left++)]++;
     }
     if (right - left === p.length) {
-      result.push(left + 1);
+      result.push(left);
     }
   }
   return result;
 
-  function charCodeAt(str, index) {
-    return str.charCodeAt(index) - 97;
+  function charCodeAt(s, i) {
+    return s.charCodeAt(i) - 97;
   }
 }

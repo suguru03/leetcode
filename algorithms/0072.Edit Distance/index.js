bea655aa29993aa8af6153627cbaa30a59688623
Suguru Motegi <suguru.motegi@gmail.com>
2020-05-31 17:05:18 -0700

    Add 0072.Edit Distance

--- algorithms/0072.Edit Distance/index.js
+++ algorithms/0072.Edit Distance/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = { minDistance };
+module.exports = { minDistance, minDistance2 };
 
 /**
  * @param {string} word1
@@ -29,4 +29,24 @@ function minDistance(word1, word2) {
     }
   }
   return dp[l1][l2];
+}
+
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number} */
+function minDistance2(word1, word2) {
+  if (Math.min(word1.length, word2.length) === 0) {
+    return Math.max(word1.length, word2.length);
+  }
+  const dp = Array.from(word1, () => Array.from(word2));
+  for (let i = 0; i < word1.length; i++) {
+    for (let j = 0; j < word2.length; j++) {
+      const used = j === 0 ? Infinity : dp[i][j - 1] + 1;
+      const del = i === 0 ? Infinity : dp[i - 1][j] + 1;
+      const take = (i === 0 || j === 0 ? i + j : dp[i - 1][j - 1]) + Number(word1[i] !== word2[j]);
+      dp[i][j] = Math.min(used, del, take);
+    }
+  }
+  return dp[word1.length - 1][word2.length - 1];
 }
--- algorithms/0072.Edit Distance/test.js
+++ algorithms/0072.Edit Distance/test.js
@@ -2,6 +2,7 @@
 
 const assert = require('assert');
 const _ = require('lodash');
+
 const { minDistance } = require('./');
 
 describe('#minDistance', () => {
@@ -12,9 +13,49 @@ describe('#minDistance', () => {
       result: 3,
     },
     {
+      word1: 'intention',
+      word2: 'execution',
+      result: 5,
+    },
+    {
+      word1: 'zoolo',
+      word2: 'zoo',
+      result: 2,
+    },
+    {
       word1: 'zoologicoarchaeologist',
       word2: 'zoogeologist',
       result: 10,
+    },
+    {
+      word1: '',
+      word2: '',
+      result: 0,
+    },
+    {
+      word1: '',
+      word2: 'abc',
+      result: 3,
+    },
+    {
+      word1: 'abc',
+      word2: '',
+      result: 3,
+    },
+    {
+      word1: 'ab',
+      word2: 'a',
+      result: 1,
+    },
+    {
+      word1: 'park',
+      word2: 'spake',
+      result: 3,
+    },
+    {
+      word1: 'pneumonoultramicroscopicsilicovolcanoconiosis',
+      word2: 'ultramicroscopically',
+      result: 27,
     },
   ];
 

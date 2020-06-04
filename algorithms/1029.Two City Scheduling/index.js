e3b2bf3c4f3af786c28a2fecca9fe335a634a60d
Suguru Motegi <suguru.motegi@gmail.com>
2020-06-03 21:00:38 -0700

    Update 1029.Two City Scheduling

--- algorithms/1029.Two City Scheduling/index.js
+++ algorithms/1029.Two City Scheduling/index.js
@@ -6,4 +6,29 @@ module.exports = { twoCitySchedCost };
  * @param {number[][]} costs
  * @return {number}
  */
-function twoCitySchedCost(costs) {}
+function twoCitySchedCost(costs) {
+  costs.sort(([a1, b1], [a2, b2]) => Math.abs(a2 - b2) - Math.abs(a1 - b1));
+  let max = 0;
+  let countA = costs.length / 2;
+  let countB = costs.length / 2;
+  for (const [ca, cb] of costs) {
+    if (countA === 0) {
+      countB--;
+      max += cb;
+      continue;
+    }
+    if (countB === 0) {
+      countA--;
+      max += ca;
+      continue;
+    }
+    if (ca < cb) {
+      countA--;
+      max += ca;
+    } else {
+      countB--;
+      max += cb;
+    }
+  }
+  return max;
+}
--- algorithms/1029.Two City Scheduling/test.js
+++ algorithms/1029.Two City Scheduling/test.js
@@ -3,38 +3,7 @@
 const assert = require('assert');
 const _ = require('lodash');
 
-// const { twoCitySchedCost } = require('./');
-
-/**
- * @param {number[][]} costs
- * @return {number}
- */
-function twoCitySchedCost(costs) {
-  costs.sort(([a1, b1], [a2, b2]) => Math.abs(a2 - b2) - Math.abs(a1 - b1));
-  let max = 0;
-  let countA = costs.length / 2;
-  let countB = costs.length / 2;
-  for (const [ca, cb] of costs) {
-    if (countA === 0) {
-      countB--;
-      max += cb;
-      continue;
-    }
-    if (countB === 0) {
-      countA--;
-      max += ca;
-      continue;
-    }
-    if (ca < cb) {
-      countA--;
-      max += ca;
-    } else {
-      countB--;
-      max += cb;
-    }
-  }
-  return max;
-}
+const { twoCitySchedCost } = require('./');
 
 describe('#twoCitySchedCost', () => {
   const tests = [

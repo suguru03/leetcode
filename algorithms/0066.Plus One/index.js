270f9a210e53db9cf5d65da193ea8b9fa6985689
Suguru Motegi <suguru.motegi@gmail.com>
2020-07-07 20:22:19 -0700

    Update 0066.Plus One

--- algorithms/0066.Plus One/index.js
+++ algorithms/0066.Plus One/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = { plusOne };
+module.exports = { plusOne, plusOne2 };
 
 /**
  * @param {number[]} digits
@@ -19,4 +19,22 @@ function plusOne(digits) {
     result.unshift(prev);
   }
   return result;
+}
+
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+function plusOne2(digits) {
+  let l = digits.length;
+  let prev = 1;
+  while (--l >= 0 && prev === 1) {
+    const sum = digits[l] + prev;
+    digits[l] = sum % 10;
+    prev = (sum / 10) | 0;
+  }
+  if (prev) {
+    digits.unshift(prev);
+  }
+  return digits;
 }

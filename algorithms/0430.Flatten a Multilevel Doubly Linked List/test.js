183a8e912bfa89e34f89771ba4140171b37a7c43
Suguru Motegi <suguru.motegi@gmail.com>
2021-01-14 10:55:22 -0800

    Fix test cases

--- algorithms/0430.Flatten a Multilevel Doubly Linked List/test.js
+++ algorithms/0430.Flatten a Multilevel Doubly Linked List/test.js
@@ -6,27 +6,11 @@ const _ = require('lodash');
 const { flatten } = require('./');
 
 describe('#hammingDistance', () => {
-  const tests = [
-    {
-      x: 1,
-      y: 4,
-      result: 2,
-    },
-    {
-      x: 1,
-      y: 1,
-      result: 0,
-    },
-    {
-      x: 1,
-      y: 15,
-      result: 3,
-    },
-  ];
+  const tests = [];
 
-  _.forEach(tests, ({ x, y, result }) => {
-    it(`${x}, ${y} -> ${result}`, () => {
-      assert.deepStrictEqual(hammingDistance(x, y), result);
+  _.forEach(tests, ({ head, _head, result, _result }) => {
+    it(`${_head} -> ${_result}`, () => {
+      assert.deepStrictEqual(flatten(head), result);
     });
   });
 });

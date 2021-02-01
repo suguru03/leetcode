1106c85e997c2775c4acadb13afb7fcf5a5fa8db
Suguru Motegi <suguru.motegi@gmail.com>
2021-02-01 10:29:18 -0800

    Update 0191.Number of 1 Bits

--- algorithms/0191.Number of 1 Bits/solution.rs
+++ algorithms/0191.Number of 1 Bits/solution.rs
@@ -4,8 +4,8 @@ impl Solution {
     pub fn hammingWeight(mut n: u32) -> i32 {
         let mut count = 0;
         while n > 0 {
-            count += n & 1;
-            n >>= 1;
+            count += 1;
+            n &= n - 1;
         }
         count as i32
     }
@@ -13,15 +13,18 @@ impl Solution {
 
 #[test]
 fn test1() {
-    assert_eq!(Solution::hammingWeight(11), 3);
+    assert_eq!(Solution::hammingWeight(0b1011), 3);
 }
 
 #[test]
 fn test2() {
-    assert_eq!(Solution::hammingWeight(2147483647), 31);
+    assert_eq!(Solution::hammingWeight(0b1000_0000), 1);
 }
 
 #[test]
 fn test3() {
-    assert_eq!(Solution::hammingWeight(2147483648), 1);
+    assert_eq!(
+        Solution::hammingWeight(0b1111_1111_1111_1111_1111_1111_1111_1101),
+        31
+    );
 }

06e006e6ec9e1c3ae375da577d8dd34afb47c786
Suguru Motegi <suguru.motegi@gmail.com>
2021-02-26 12:19:26 -0800

    Add 0946.Validate Stack Sequences [rs]

---
+++ algorithms/0946.Validate Stack Sequences/solution.rs
@@ -0,0 +1,37 @@
+pub struct Solution;
+
+use std::collections::VecDeque;
+
+impl Solution {
+    pub fn validate_stack_sequences(pushed: Vec<i32>, popped: Vec<i32>) -> bool {
+        let mut stack = VecDeque::new();
+        let mut i = 0;
+        for n in pushed {
+            stack.push_back(n);
+            while let Some(head) = stack.back() {
+                if head != &popped[i] {
+                    break;
+                }
+                stack.pop_back();
+                i += 1;
+            }
+        }
+        stack.is_empty()
+    }
+}
+
+#[test]
+fn test1() {
+    assert_eq!(
+        Solution::validate_stack_sequences(vec![1, 2, 3, 4, 5], vec![4, 5, 3, 2, 1]),
+        true
+    );
+}
+
+#[test]
+fn test2() {
+    assert_eq!(
+        Solution::validate_stack_sequences(vec![1, 2, 3, 4, 5], vec![4, 5, 3, 1, 2]),
+        false
+    );
+}

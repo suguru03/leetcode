3687f1b8190709797499bfee57372d9c72be1278
Suguru Motegi <suguru.motegi@gmail.com>
2021-12-26 22:15:40 -0800

    Update 0106.Construct Binary Tree from Inorder and Postorder Traversal [js]

--- algorithms/0106.Construct Binary Tree from Inorder and Postorder Traversal/index.js
+++ algorithms/0106.Construct Binary Tree from Inorder and Postorder Traversal/index.js
@@ -7,9 +7,9 @@ const { TreeNode } = require('../util');
 /**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
+ *  this.val = (val===undefined ? 0 : val)
+ *  this.left = (left===undefined ? null : left)
+ *  this.right = (right===undefined ? null : right)
  * }
  */
 /**

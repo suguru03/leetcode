db0503288e0f89cb2b0c7c329fe7c48cec98dde5
Suguru Motegi <suguru.motegi@gmail.com>
2021-12-30 22:56:57 -0800

    Update 0889.Construct Binary Tree from Preorder and Postorder Traversal [js]

--- algorithms/0889.Construct Binary Tree from Preorder and Postorder Traversal/index.js
+++ algorithms/0889.Construct Binary Tree from Preorder and Postorder Traversal/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = { constructFromPrePost };
+module.exports = { constructFromPrePost, constructFromPrePost2 };
 
 const { TreeNode } = require('../utils');
 
@@ -31,4 +31,36 @@ function constructFromPrePost(preorder, postorder) {
   node.left = constructFromPrePost(preorder.slice(0, index), postorder.slice(0, index));
   node.right = constructFromPrePost(preorder.slice(index), postorder.slice(index));
   return node;
+}
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *   this.val = (val===undefined ? 0 : val)
+ *   this.left = (left===undefined ? null : left)
+ *   this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @see https://leetcode.com/problems/construct-binary-tree-from-preorder-and-postorder-traversal/discuss/161268/C%2B%2BJavaPython-One-Pass-Real-O(N)
+ * @param {number[]} preorder
+ * @param {number[]} postorder
+ * @return {TreeNode}
+ */
+function constructFromPrePost2(preorder, postorder) {
+  let preIndex = 0;
+  let postIndex = 0;
+  return dfs();
+
+  function dfs() {
+    const node = new TreeNode(preorder[preIndex++]);
+    if (node.val !== postorder[postIndex]) {
+      node.left = dfs();
+    }
+    if (node.val !== postorder[postIndex]) {
+      node.right = dfs();
+    }
+    postIndex++;
+    return node;
+  }
 }

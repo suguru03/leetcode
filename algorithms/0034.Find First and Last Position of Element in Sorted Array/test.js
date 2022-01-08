adc49d92dda0f872d247e91647e62b35348d921f
Suguru Motegi <suguru.motegi@gmail.com>
2022-01-07 19:48:18 -0800

    Update 0034.Find First and Last Position of Element in Sorted Array [js]

--- algorithms/0034.Find First and Last Position of Element in Sorted Array/test.js
+++ algorithms/0034.Find First and Last Position of Element in Sorted Array/test.js
@@ -2,47 +2,7 @@
 
 const assert = require('assert');
 
-// const { searchRange } = require('./');
-
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
- */
-function searchRange(nums, target) {
-  const left = findLeft(nums, target);
-  return [left, findRight(nums, target, left)];
-}
-
-function findLeft(nums, target) {
-  let l = 0;
-  let r = nums.length;
-  while (l < r) {
-    const m = (l + (r - l) / 2) | 0;
-    if (nums[m] < target) {
-      l = m + 1;
-    } else {
-      r = m;
-    }
-  }
-  return nums[l] === target ? l : -1;
-}
-
-function findRight(nums, target, l) {
-  if (l === -1) {
-    return -1;
-  }
-  let r = nums.length;
-  while (l + 1 < r) {
-    const m = (l + (r - l) / 2) | 0;
-    if (nums[m] <= target) {
-      l = m;
-    } else {
-      r = m;
-    }
-  }
-  return l;
-}
+const { searchRange } = require('./');
 
 describe('#searchRange', () => {
   const tests = [

05dcbe3c05cfd617bbcadb20470be10bfb12a755
Suguru Motegi <suguru.motegi@gmail.com>
2022-01-09 16:15:26 -0800

    Update 0350.Intersection of Two Arrays II [js]

--- algorithms/0350.Intersection of Two Arrays II/index.js
+++ algorithms/0350.Intersection of Two Arrays II/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = { intersect };
+module.exports = { intersect, intersect2 };
 
 /**
  * @param {number[]} nums1
@@ -24,4 +24,32 @@ function intersect(nums1, nums2) {
     }
   }
   return result;
+}
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+function intersect2(nums1, nums2) {
+  nums1.sort((n1, n2) => n1 - n2);
+  let l = 0;
+  return nums2
+    .sort((n1, n2) => n1 - n2)
+    .filter((n) => {
+      let r = nums1.length;
+      while (l < r) {
+        const m = (l + (r - l) / 2) | 0;
+        if (nums1[m] < n) {
+          l = m + 1;
+        } else {
+          r = m;
+        }
+      }
+      if (nums1[l] !== n) {
+        return false;
+      }
+      l++;
+      return true;
+    });
 }
--- algorithms/0350.Intersection of Two Arrays II/test.js
+++ algorithms/0350.Intersection of Two Arrays II/test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const assert = require('assert');
-const _ = require('lodash');
+
 const { intersect } = require('./');
 
 describe('#intersect', () => {
@@ -13,9 +13,9 @@ describe('#intersect', () => {
     },
   ];
 
-  _.forEach(tests, ({ nums1, nums2, result }) => {
+  for (const { nums1, nums2, result } of tests) {
     it(`${nums1}, ${nums2} -> ${result}`, () => {
-      assert.deepEqual(intersect(nums1, nums2), result);
+      assert.deepStrictEqual(intersect(nums1, nums2), result);
     });
-  });
+  }
 });

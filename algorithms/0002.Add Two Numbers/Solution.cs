8222cb4f320d1994bab4da9881900f4c73845fb3
Suguru Motegi <suguru.motegi@gmail.com>
2022-09-04 13:01:44 -0700

    Update 0002.Add Two Numbers [cs]

--- algorithms/0002.Add Two Numbers/Solution.cs
+++ algorithms/0002.Add Two Numbers/Solution.cs
@@ -36,5 +36,42 @@ namespace AddTwoNumbers
 
             return head.next;
         }
+
+        public ListNode AddTwoNumbers2(ListNode l1, ListNode l2)
+        {
+            var head = l1;
+            var cur = l1;
+            var nextVal = 0;
+            while (l1 != null && l2 != null)
+            {
+                cur = l1;
+                l1.val += l2.val + nextVal;
+                nextVal = l1.val / 10;
+                l1.val %= 10;
+                l1 = l1.next;
+                l2 = l2.next;
+            }
+
+            if (l2 != null)
+            {
+                cur.next = l2;
+            }
+
+            while (nextVal != 0)
+            {
+                if (cur.next == null)
+                {
+                    cur.next = new ListNode();
+                }
+
+                cur = cur.next;
+                cur.val += nextVal;
+                nextVal = cur.val / 10;
+                cur.val %= 10;
+            }
+
+            return head;
+        }
+
     }
 }

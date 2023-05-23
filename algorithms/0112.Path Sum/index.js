05ba1d4e344e1042d497cb8df3bf8f120c76ee0a
Suguru Motegi <suguru.motegi@gmail.com>
2023-05-22 18:43:50 -0700

    Update 0112.Path Sum [js]

--- algorithms/0112.Path Sum/index.js
+++ algorithms/0112.Path Sum/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = { hasPathSum };
+module.exports = { hasPathSum, hasPathSum2 };
 
 /**
  * Definition for a binary tree node.
@@ -21,4 +21,25 @@ function hasPathSum(node, sum) {
   const { left, right, val } = node;
   sum -= val;
   return (left === right && sum === 0) || hasPathSum(left, sum) || hasPathSum(right, sum);
+}
+
+function hasPathSum2(node, sum) {
+  const queue = [[node, sum]];
+  while (queue.length !== 0) {
+    let [node, sum] = queue.shift();
+    if (node === null) {
+      continue;
+    }
+    const { left, right, val } = node;
+    sum -= val;
+    if (left === right) {
+      if (sum === 0) {
+        return true;
+      }
+      continue;
+    }
+    queue.push([left, sum], [right, sum]);
+  }
+
+  return false;
 }

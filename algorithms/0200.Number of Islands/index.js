d45f753be3453bdcb81e0fa65ebce3f9b93157c1
Suguru Motegi <suguru.motegi@gmail.com>
2023-05-22 18:58:10 -0700

    Update 0200.Number of Islands [js]

--- algorithms/0200.Number of Islands/index.js
+++ algorithms/0200.Number of Islands/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = { numIslands };
+module.exports = { numIslands, numIslands2 };
 
 /**
  * @param {character[][]} grid
@@ -30,4 +30,30 @@ function numIslands(grid) {
     dfs(x, y - 1);
     dfs(x, y + 1);
   }
+}
+
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+function numIslands2(grid) {
+  let result = 0;
+  for (const [y, row] of grid.entries()) {
+    for (const [x, n] of row.entries()) {
+      if (n === '0') {
+        continue;
+      }
+      result++;
+      const queue = [[x, y]];
+      while (queue.length !== 0) {
+        const [x, y] = queue.shift();
+        if (grid[y]?.[x] !== '1') {
+          continue;
+        }
+        grid[y][x] = '0';
+        queue.push([x - 1, y], [x, y - 1], [x + 1, y], [x, y + 1]);
+      }
+    }
+  }
+  return result;
 }

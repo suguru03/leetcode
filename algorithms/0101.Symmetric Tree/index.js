e38c283b72c62f5c7f50d6fbd2e1bb234f08eb57
Suguru Motegi <suguru.motegi@gmail.com>
2023-05-30 18:08:32 -0700

    Update 0101.Symmetric Tree [js]

--- algorithms/0101.Symmetric Tree/index.js
+++ algorithms/0101.Symmetric Tree/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = { isSymmetric };
+module.exports = { isSymmetric, isSymmetric2 };
 
 /**
  * Definition for a binary tree node.
@@ -29,4 +29,29 @@ function isSymmetric(root) {
     queue.push(right.right, right.left);
   }
   return true;
+}
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+function isSymmetric2(root) {
+  return dfs(root?.left, root?.right);
+  function dfs(left, right) {
+    if (left == null || right == null) {
+      return left === right;
+    }
+    if (left.val !== right.val) {
+      return false;
+    }
+    return dfs(left.left, right.right) && dfs(left.right, right.left);
+  }
 }

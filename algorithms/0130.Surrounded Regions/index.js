cf3b3de3f306fe6e0337ae414c043c85983cb115
Suguru Motegi <suguru.motegi@gmail.com>
2023-05-30 19:19:14 -0700

    Update 0130.Surrounded Regions [js]

--- algorithms/0130.Surrounded Regions/index.js
+++ algorithms/0130.Surrounded Regions/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = { solve };
+module.exports = { solve, solve2 };
 
 /**
  * @param {character[][]} board
@@ -14,7 +14,7 @@ function solve(board) {
   let xl = board[0].length;
   let groupCount = 0;
   const surroundedSet = new Set();
-  const marked = Array.from(board, row => Array.from(row).fill(0));
+  const marked = Array.from(board, (row) => Array.from(row).fill(0));
   for (let y = 0; y < yl; y++) {
     for (let x = 0; x < xl; x++) {
       if (board[y][x] === 'X') {
@@ -45,5 +45,43 @@ function solve(board) {
     }
     marked[y][x] = group;
     return dfs(x - 1, y, group) && dfs(x + 1, y, group) && dfs(x, y - 1, group) && dfs(x, y + 1, group);
+  }
+}
+
+/**
+ * @param {character[][]} board
+ * @return {void} Do not return anything, modify board in-place instead.
+ */
+function solve2(board) {
+  const seen = Array.from(board, (row) => Array(row.length));
+  for (let y = 1; y < board.length - 1; y++) {
+    for (let x = 1; x < board[y].length - 1; x++) {
+      if (seen[y][x]) {
+        const [x0, y0] = seen;
+        board[y][x] = board[y0][x0];
+        continue;
+      }
+
+      if (check(x, y, [x, y])) {
+        board[y][x] = 'X';
+      }
+    }
+  }
+
+  function check(x, y, group) {
+    if (y < 0 || y >= board.length || x < 0 || x >= board[y].length) {
+      return false;
+    }
+
+    if (board[y][x] === 'X') {
+      return true;
+    }
+
+    if (seen[y][x]) {
+      return group === seen[y][x];
+    }
+
+    seen[y][x] = group;
+    return check(x - 1, y, group) && check(x, y - 1, group) && check(x + 1, y, group) && check(x, y + 1, group);
   }
 }

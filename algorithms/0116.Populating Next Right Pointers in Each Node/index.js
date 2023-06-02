3eda90109b83f1295b31f19143749aef83f7bd48
Suguru Motegi <suguru.motegi@gmail.com>
2023-06-01 17:25:42 -0700

    Update 0116.Populating Next Right Pointers in Each Node [js]

--- algorithms/0116.Populating Next Right Pointers in Each Node/index.js
+++ algorithms/0116.Populating Next Right Pointers in Each Node/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = { connect };
+module.exports = { connect, connect2 };
 
 /**
  * // Definition for a Node.
@@ -34,4 +34,37 @@ function connect(root) {
     queue = next;
   }
   return root;
+}
+
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+function connect2(root) {
+  const nodes = [];
+  dfs(root, 0);
+  return root;
+
+  function dfs(node, level) {
+    if (node == null) {
+      return;
+    }
+
+    if (nodes[level]) {
+      nodes[level].next = node;
+    }
+    nodes[level++] = node;
+    dfs(node.left, level);
+    dfs(node.right, level);
+  }
 }

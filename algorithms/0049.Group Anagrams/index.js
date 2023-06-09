b65e38e0dc2f1f832a6ff95a87d4325567bebc71
Suguru Motegi <suguru.motegi@gmail.com>
2023-06-08 18:46:52 -0700

    Update 0049.Group Anagrams [js]

--- algorithms/0049.Group Anagrams/index.js
+++ algorithms/0049.Group Anagrams/index.js
@@ -1,21 +1,71 @@
 'use strict';
 
-module.exports = { groupAnagrams };
+module.exports = { groupAnagrams, groupAnagrams2 };
 
 /**
  * @param {string[]} strs
  * @return {string[][]}
  */
 function groupAnagrams(strs) {
-  const groupMap = new Map();
+  const group = new Map();
   for (const str of strs) {
-    const counts = Array(26).fill(0);
+    const chars = Array(26).fill(0);
     for (const c of str) {
-      counts[c.charCodeAt() - 97]++;
+      chars[c.charCodeAt(0) - 97]++;
     }
-    const code = counts.join('');
-    groupMap.set(code, groupMap.get(code) || []);
-    groupMap.get(code).push(str);
+    const key = chars.join(',');
+    group.set(key, group.get(key) ?? []);
+    group.get(key).push(str);
   }
-  return Array.from(groupMap.values());
+
+  return Array.from(group.values());
+}
+
+const primes = [
+  2,
+  3,
+  5,
+  7,
+  11,
+  13,
+  17,
+  19,
+  23,
+  29,
+  31,
+  37,
+  41,
+  43,
+  47,
+  53,
+  59,
+  61,
+  67,
+  71,
+  73,
+  79,
+  83,
+  89,
+  97,
+  101,
+  107,
+];
+
+/**
+ * @see https://leetcode.com/problems/group-anagrams/solutions/19233/o-m-n-algorithm-using-hash-without-sort/
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+function groupAnagrams2(strs) {
+  const group = new Map();
+  for (const str of strs) {
+    let key = 1;
+    for (const c of str) {
+      key *= primes[c.charCodeAt(0) - 97];
+    }
+    group.set(key, group.get(key) ?? []);
+    group.get(key).push(str);
+  }
+
+  return Array.from(group.values());
 }

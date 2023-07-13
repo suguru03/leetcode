a526672310cc71d948516a8cd2e5e2974b79ee9d
Suguru Motegi <suguru.motegi@gmail.com>
2023-07-13 09:09:56 -0700

    Update 0210.Course Schedule II [js]

--- algorithms/0210.Course Schedule II/test.js
+++ algorithms/0210.Course Schedule II/test.js
@@ -14,17 +14,38 @@ describe('#findOrder', () => {
     },
     {
       numCourses: 4,
-      prerequisites: [[1, 0], [2, 0], [3, 1], [3, 2]],
+      prerequisites: [
+        [1, 0],
+        [2, 0],
+        [3, 1],
+        [3, 2],
+      ],
       result: [0, 2, 1, 3],
     },
     {
       numCourses: 2,
-      prerequisites: [[0, 1], [1, 0]],
+      prerequisites: [
+        [0, 1],
+        [1, 0],
+      ],
       result: [],
     },
     {
       numCourses: 3,
-      prerequisites: [[1, 0], [0, 2], [2, 1]],
+      prerequisites: [
+        [1, 0],
+        [0, 2],
+        [2, 1],
+      ],
+      result: [],
+    },
+    {
+      numCourses: 3,
+      prerequisites: [
+        [1, 0],
+        [1, 2],
+        [0, 1],
+      ],
       result: [],
     },
   ];

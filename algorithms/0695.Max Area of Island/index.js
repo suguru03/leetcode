da38e56c3689f2ff49648e500e320e73b97e1d2d
Suguru Motegi <suguru.motegi@gmail.com>
2024-07-03 09:59:37 -0700

    Update 0695.Max Area of Island [js]

--- algorithms/0695.Max Area of Island/index.js
+++ algorithms/0695.Max Area of Island/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = { maxAreaOfIsland };
+module.exports = { maxAreaOfIsland, maxAreaOfIsland2 };
 
 /**
  * @param {number[][]} grid
@@ -24,5 +24,42 @@ function maxAreaOfIsland(grid) {
     }
     grid[y][x] = 0;
     return dfs(x - 1, y) + dfs(x + 1, y) + dfs(x, y - 1) + dfs(x, y + 1) + 1;
+  }
+}
+
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+function maxAreaOfIsland2(grid) {
+  let max = 0;
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[y].length; x++) {
+      max = Math.max(max, bfs(x, y));
+    }
+  }
+  return max;
+
+  function bfs(x, y) {
+    let count = 0;
+    if (grid[y][x] === 0) {
+      return count;
+    }
+
+    const queue = [[x, y]];
+    while (queue.length > 0) {
+      const [x, y] = queue.shift();
+      if (y < 0 || y >= grid.length || x < 0 || x >= grid[y].length || grid[y][x] === 0) {
+        continue;
+      }
+      count++;
+      grid[y][x] = 0;
+      queue.push([x - 1, y]);
+      queue.push([x, y - 1]);
+      queue.push([x + 1, y]);
+      queue.push([x, y + 1]);
+    }
+
+    return count;
   }
 }

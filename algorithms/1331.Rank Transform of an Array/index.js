a6d277fdc05b6a3c5ada1cb5b2cc22a137fbf095
Suguru Motegi <suguru.motegi@gmail.com>
2024-10-02 15:56:33 -0700

    Update 1331.Rank Transform of an Array [js]

--- algorithms/1331.Rank Transform of an Array/index.js
+++ algorithms/1331.Rank Transform of an Array/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = { arrayRankTransform };
+module.exports = { arrayRankTransform, arrayRankTransform2 };
 
 /**
  * @param {number[]} arr
@@ -12,5 +12,20 @@ function arrayRankTransform(arr) {
       .sort((n1, n2) => n1 - n2)
       .map((num, rank) => [num, rank + 1]),
   );
-  return arr.map(num => rankMap.get(num));
+  return arr.map((num) => rankMap.get(num));
+}
+
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+function arrayRankTransform2(arr) {
+  const map = new Map(
+    _.chain(arr)
+      .uniq()
+      .sortBy()
+      .map((n, i) => [n, i + 1])
+      .value(),
+  );
+  return arr.map((n) => map.get(n));
 }
